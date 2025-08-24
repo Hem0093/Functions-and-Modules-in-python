0796078a8cf6c21f29bdfdbddda0d162782dff4b
Hem0093 <hemantdevatwalhk@gmail.com>
2025-08-24 10:32:29 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,17 @@
+import math
+
+n = int(input("Enter a number:"))
+
+square = math.sqrt(n)
+
+
+n = int(input("Enter a number:"))
+algorithm = math.log(n)
+
+
+n = int(input("Enter a number:"))
+sinc = math.sin(n)
+
+print("square root:",square)
+print("logarithm :",algorithm)
+print("sinc :",sinc)
